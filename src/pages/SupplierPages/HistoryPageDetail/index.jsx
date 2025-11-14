0bed0f116dbfae3053c2299ff7744c6299ebc448
AnarZulfuqarov <anarrz-af106@code.edu.az>
2025-11-14 16:06:26 +0400

    Crm

--- src/pages/SupplierPages/HistoryPageDetail/index.jsx
+++ src/pages/SupplierPages/HistoryPageDetail/index.jsx
@@ -87,7 +87,13 @@ const OrderHistoryDetailSuplier = () => {
       }) || [];
 
   const isMobile = window.innerWidth <= 768;
-  return (
+    const groupedByCategory = filtered?.reduce((acc, item) => {
+        if (!acc[item.category]) acc[item.category] = [];
+        acc[item.category].push(item);
+        return acc;
+    }, {});
+
+    return (
     <div className="order-history-detail-main-supplier">
       <div className="order-history-detail-supplier">
         <h2>
@@ -183,6 +189,41 @@ const OrderHistoryDetailSuplier = () => {
             <table className="order-history-detail-supplier__table">
               <thead>
                 <tr>
+                    <th>
+                        {activeSearch === "category" ? (
+                            <div className="th-search">
+                                <input
+                                    autoFocus
+                                    value={searchCategory}
+                                    onChange={(e) => setSearchCategory(e.target.value)}
+                                    placeholder="Axtar..."
+                                />
+                                <FaTimes
+                                    onClick={() => {
+                                        setActiveSearch(null);
+                                        setSearchCategory("");
+                                    }}
+                                />
+                            </div>
+                        ) : (
+                            <div className="th-label">
+                                Kateqoriyası
+                                <svg
+                                    onClick={() => setActiveSearch("category")}
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                >
+                                    <path
+                                        d="M20.71 19.29L17.31 15.9C18.407 14.5025 19.0022 12.7767 19 11C19 9.41775 18.5308 7.87103 17.6518 6.55544C16.7727 5.23985 15.5233 4.21447 14.0615 3.60897C12.5997 3.00347 10.9911 2.84504 9.43928 3.15372C7.88743 3.4624 6.46197 4.22433 5.34315 5.34315C4.22433 6.46197 3.4624 7.88743 3.15372 9.43928C2.84504 10.9911 3.00347 12.5997 3.60897 14.0615C4.21447 15.5233 5.23985 16.7727 6.55544 17.6518C7.87103 18.5308 9.41775 19 11 19C12.7767 19.0022 14.5025 18.407 15.9 17.31L19.29 20.71C19.383 20.8037 19.4936 20.8781 19.6154 20.9289C19.7373 20.9797 19.868 21.0058 20 21.0058C20.132 21.0058 20.2627 20.9797 20.3846 20.9289C20.5064 20.8781 20.617 20.8037 20.71 20.71C20.8037 20.617 20.8781 20.5064 20.9289 20.3846C20.9797 20.2627 21.0058 20.132 21.0058 20C21.0058 19.868 20.9797 19.7373 20.9289 19.6154C20.8781 19.4936 20.8037 19.383 20.71 19.29ZM5 11C5 9.81332 5.3519 8.65328 6.01119 7.66658C6.67047 6.67989 7.60755 5.91085 8.7039 5.45673C9.80026 5.0026 11.0067 4.88378 12.1705 5.11529C13.3344 5.3468 14.4035 5.91825 15.2426 6.75736C16.0818 7.59648 16.6532 8.66558 16.8847 9.82946C17.1162 10.9933 16.9974 12.1997 16.5433 13.2961C16.0892 14.3925 15.3201 15.3295 14.3334 15.9888C13.3467 16.6481 12.1867 17 11 17C9.4087 17 7.88258 16.3679 6.75736 15.2426C5.63214 14.1174 5 12.5913 5 11Z"
+                                        fill="#7A7A7A"
+                                    />
+                                </svg>
+                            </div>
+                        )}
+                    </th>
                   <th>
                     {activeSearch === "name" ? (
                       <div className="th-search">
@@ -218,41 +259,7 @@ const OrderHistoryDetailSuplier = () => {
                       </div>
                     )}
                   </th>
-                  <th>
-                    {activeSearch === "category" ? (
-                      <div className="th-search">
-                        <input
-                          autoFocus
-                          value={searchCategory}
-                          onChange={(e) => setSearchCategory(e.target.value)}
-                          placeholder="Axtar..."
-                        />
-                        <FaTimes
-                          onClick={() => {
-                            setActiveSearch(null);
-                            setSearchCategory("");
-                          }}
-                        />
-                      </div>
-                    ) : (
-                      <div className="th-label">
-                        Kateqoriyası
-                        <svg
-                          onClick={() => setActiveSearch("category")}
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                        >
-                          <path
-                            d="M20.71 19.29L17.31 15.9C18.407 14.5025 19.0022 12.7767 19 11C19 9.41775 18.5308 7.87103 17.6518 6.55544C16.7727 5.23985 15.5233 4.21447 14.0615 3.60897C12.5997 3.00347 10.9911 2.84504 9.43928 3.15372C7.88743 3.4624 6.46197 4.22433 5.34315 5.34315C4.22433 6.46197 3.4624 7.88743 3.15372 9.43928C2.84504 10.9911 3.00347 12.5997 3.60897 14.0615C4.21447 15.5233 5.23985 16.7727 6.55544 17.6518C7.87103 18.5308 9.41775 19 11 19C12.7767 19.0022 14.5025 18.407 15.9 17.31L19.29 20.71C19.383 20.8037 19.4936 20.8781 19.6154 20.9289C19.7373 20.9797 19.868 21.0058 20 21.0058C20.132 21.0058 20.2627 20.9797 20.3846 20.9289C20.5064 20.8781 20.617 20.8037 20.71 20.71C20.8037 20.617 20.8781 20.5064 20.9289 20.3846C20.9797 20.2627 21.0058 20.132 21.0058 20C21.0058 19.868 20.9797 19.7373 20.9289 19.6154C20.8781 19.4936 20.8037 19.383 20.71 19.29ZM5 11C5 9.81332 5.3519 8.65328 6.01119 7.66658C6.67047 6.67989 7.60755 5.91085 8.7039 5.45673C9.80026 5.0026 11.0067 4.88378 12.1705 5.11529C13.3344 5.3468 14.4035 5.91825 15.2426 6.75736C16.0818 7.59648 16.6532 8.66558 16.8847 9.82946C17.1162 10.9933 16.9974 12.1997 16.5433 13.2961C16.0892 14.3925 15.3201 15.3295 14.3334 15.9888C13.3467 16.6481 12.1867 17 11 17C9.4087 17 7.88258 16.3679 6.75736 15.2426C5.63214 14.1174 5 12.5913 5 11Z"
-                            fill="#7A7A7A"
-                          />
-                        </svg>
-                      </div>
-                    )}
-                  </th>
+
                   <th>Tələb olunan miqdar</th>
                   <th>Təmin olunan miqdar</th>
                   <th>Sifarişin məbləği</th>
@@ -265,64 +272,63 @@ const OrderHistoryDetailSuplier = () => {
                   <th>İnvoysa Bax</th>
                 </tr>
               </thead>
-              <tbody>
-                {filtered.map((item, i) => {
-                  // Sayı kimi müqayisə üçün rəqəmləri çıxarırıq
-                  const requiredQty = parseFloat(item.required);
-                  const providedQty = parseFloat(item.provided);
+                <tbody>
+                {Object.entries(groupedByCategory || {}).map(([category, items]) => (
+                    <>
 
-                  const rowStyle =
-                    requiredQty > providedQty
-                      ? { backgroundColor: "rgba(255, 0, 0, 0.08)" } // Açıq qırmızı fon
-                      : {};
+                        {/* BU KATEQORIYANIN ITEM-LƏRI */}
+                        {items.map((item, i) => {
+                            const requiredQty = parseFloat(item.required);
+                            const providedQty = parseFloat(item.provided);
 
-                  return (
-                    <tr key={i} style={rowStyle}>
-                      <td>{item.name}</td>
-                      <td>{item.category}</td>
-                      <td>{item.required}</td>
-                      <td>{item.provided}</td>
-                      <td>{item.price}</td>
-                      <td>{item.priceEach}</td>
-                      <td>{item.vendorName}</td>
-                      <td>{item.created}</td>
-                      <td>{item.delivery}</td>
-                      <td>{item.received}</td>
+                            const rowStyle =
+                                requiredQty > providedQty
+                                    ? { backgroundColor: "rgba(255, 0, 0, 0.08)" }
+                                    : {};
 
-                      <td
-                        style={{
-                          textAlign: "center",
-                        }}
-                      >
-                        <svg
-                          onClick={() => setIsOverheadModalOpen(true)}
-                          style={{ cursor: "pointer" }}
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="21"
-                          height="20"
-                          viewBox="0 0 21 20"
-                          fill="none"
-                        >
-                          <path
-                            d="M13 10C13 10.663 12.7366 11.2989 12.2678 11.7678C11.7989 12.2366 11.163 12.5 10.5 12.5C9.83696 12.5 9.20107 12.2366 8.73223 11.7678C8.26339 11.2989 8 10.663 8 10C8 9.33696 8.26339 8.70107 8.73223 8.23223C9.20107 7.76339 9.83696 7.5 10.5 7.5C11.163 7.5 11.7989 7.76339 12.2678 8.23223C12.7366 8.70107 13 9.33696 13 10Z"
-                            stroke="#606060"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          />
-                          <path
-                            d="M2.16602 10.0003C3.49935 6.58616 6.61268 4.16699 10.4993 4.16699C14.386 4.16699 17.4993 6.58616 18.8327 10.0003C17.4993 13.4145 14.386 15.8337 10.4993 15.8337C6.61268 15.8337 3.49935 13.4145 2.16602 10.0003Z"
-                            stroke="#606060"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          />
-                        </svg>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
+                            return (
+                                <tr key={category + i} style={rowStyle}>
+                                    <td>{item.category}</td>
+                                    <td>{item.name}</td>
+
+                                    <td>{item.required}</td>
+                                    <td>{item.provided}</td>
+                                    <td>{item.price}</td>
+                                    <td>{item.priceEach}</td>
+                                    <td>{item.vendorName}</td>
+                                    <td>{item.created}</td>
+                                    <td>{item.delivery}</td>
+                                    <td>{item.received}</td>
+
+                                    <td style={{ textAlign: "center" }}>
+                                        <svg
+                                            onClick={() => setIsOverheadModalOpen(true)}
+                                            style={{ cursor: "pointer" }}
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="21"
+                                            height="20"
+                                            viewBox="0 0 21 20"
+                                            fill="none"
+                                        >
+                                            <path
+                                                d="M13 10C13 10.663 12.7366 11.2989 12.2678 11.7678C11.7989 12.2366 11.163 12.5 10.5 12.5C9.83696 12.5 9.20107 12.2366 8.73223 11.7678C8.26339 11.2989 8 10.663 8 10C8 9.33696 8.26339 8.70107 8.73223 8.23223C9.20107 7.76339 9.83696 7.5 10.5 7.5C11.163 7.5 11.7989 7.76339 12.2678 8.23223C12.7366 8.70107 13 9.33696 13 10Z"
+                                                stroke="#606060"
+                                                strokeWidth="1.5"
+                                            />
+                                            <path
+                                                d="M2.16602 10.0003C3.49935 6.58616 6.61268 4.16699 10.4993 4.16699C14.386 4.16699 17.4993 6.58616 18.8327 10.0003C17.4993 13.4145 14.386 15.8337 10.4993 15.8337C6.61268 15.8337 3.49935 13.4145 2.16602 10.0003Z"
+                                                stroke="#606060"
+                                                strokeWidth="1.5"
+                                            />
+                                        </svg>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </>
+                ))}
+                </tbody>
+
             </table>
           </div>
           <div className="table-footer sticky-footer">
